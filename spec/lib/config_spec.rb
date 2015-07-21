2095c7e1c8778672ec576fde957ef67c8e4af26a
Gabriel Souza <gabriel.souza@locaweb.com.br>
2015-07-21 14:29:31 -0300

    start

--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -6,6 +6,15 @@ RSpec.describe Razoul::Configuration do
       let(:config_file) {'spec/fixtures/razoul.yml'}
       specify { expect(described_class.new(config_file)).to be_kind_of Razoul::Configuration }
     end
+
+    context 'when invalid extension' do
+      specify { expect(described_class.new(config_file)).to be_thrown Razoul::Exception::InvalidExtension }
+    end
+
+    context 'when file not found' do
+      specify { expect(described_class.new(config_file)).to be_thrown Razoul::Exception::FileNotFound }
+    end
+
   end
 
 end
